73ae330b2c6e07ad1828ff200a1f2cdb360d0ecd
vives <vivesjordi96@gmail.com>
2026-09-25 20:10:43 +0200

    Commit amb la interfície bàsica llesta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('ioc-angular-bookshelfapp-jordi-vives');
+  public appName: string = "BookShelfApp";
 }
